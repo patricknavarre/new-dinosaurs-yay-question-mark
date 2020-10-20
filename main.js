63be8267240e0f048ac394c57c0d75d47fdcb30b
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-10-20 10:32:34 -0400

    8-passed

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 // Your code below.
 
+const makeDino = function(newSpecies, newPeriod, newCarnivore, newExtinct = false) {
+  return {
+    species: newSpecies,
+    period: newPeriod,
+    carnivore: newCarnivore,
+    extinct: newExtinct,
+  }
+  return newDino;
+}
+
+const makeExtinct = function(dino) {
+  const newDino = makeDino(dino.species, dino.period, dino.carnivore);
+  newDino.extinct = true;
+
+  return newDino;
+}
+
+const truncateSpecies = function (obj) {
+  if (obj.species.length > 10) {
+    return makeDino(obj.species.slice(0, 7) + "...", obj.period, obj.carnivore, obj.extinct)
+  } else {
+    return makeDino(obj.species, obj.period, obj.carnivore, obj.extinct)
+  }
+}
+
+
+
+
 
 
 
